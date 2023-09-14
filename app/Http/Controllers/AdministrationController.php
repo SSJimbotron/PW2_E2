4231c3914be8c5f02b82662acaaea3aed1961a7b
CSTJ\0957120 <jimmylee.m@outlook.com>
2023-09-14 15:07:26 -0400

    Début de la création de la page admin.index et des seeder activites et actualites

--- app/Http/Controllers/AdministrationController.php
+++ app/Http/Controllers/AdministrationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activite;
+use App\Models\Actualite;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdministrationController extends Controller
@@ -11,7 +14,17 @@ class AdministrationController extends Controller
      *
      * @return View
      */
-    public function index() {
-        return view('admin.index');
+    public function index()
+    {
+        // Récupération de tous les usagers
+        $usagers = User::all();
+        $activites = Activite::all();
+        $actualites = Actualite::all();
+
+        return view('admin.index', [
+            "usagers" => $usagers,
+            "activites" => $activites,
+            "actualites" => $actualites,
+        ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,55 @@
 <x-layout titre="Administration">
+    <h1>Administration</h1>
+
+    <div class="admin_index">
+        <div class="admin_index_usagers">
+            <h2>Usagers</h2>
+            @foreach ($usagers as $usager)
+                @if ($usager->role == 1 || $usager->role == 2)
+                    <p>{{ $usager->nom }} {{ $usager->prenom }} {{ $usager->email }}
+
+                        @if ($usager->role == 1)
+                            Client
+                        @endif
+                        @if ($usager->role == 2)
+                            Employé
+                        @endif
+                        @if ($usager->role == 3)
+                            Administrateur
+                        @endif
+                    </p>
+                @endif
+                @if (Auth::check() && (Auth::user()->role == 2 || Auth::user()->role == 3) && $usager->role == 3)
+                    <p>{{ $usager->nom }} {{ $usager->prenom }} {{ $usager->email }}
+
+                        @if ($usager->role == 1)
+                            Client
+                        @endif
+                        @if ($usager->role == 2)
+                            Employé
+                        @endif
+                        @if ($usager->role == 3)
+                            Administrateur
+                        @endif
+                    </p>
+                @endif
+            @endforeach
+        </div>
+
+        <div class="admin_index_activites">
+            <h2>Activites</h2>
+            @foreach ($activites as $activite)
+                <p>{{ $activite->nom }} {{ $activite->image }} {{ $activite->description }}</p>
+            @endforeach
+        </div>
+
+        <div class="admin_index_actualites">
+            <h2>Actualité</h2>
+            @foreach ($actualites as $actualite)
+                <p>{{ $actualite->titre }} {{ $actualite->created_at }} {{ $actualite->contenu }}</p>
+            @endforeach
+        </div>
+    </div>
+
 
 </x-layout>
