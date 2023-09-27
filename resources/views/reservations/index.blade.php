a5ad6808142e29c5409b29b1aa7359db6afed89d
CSTJ\0957120 <jimmylee.m@outlook.com>
2023-09-27 14:45:15 -0400

    Debut de la page responsive mobile A propos

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -37,9 +37,8 @@
         });
     });
 </script>
+
 <x-layout titre="Nouvelle réservation">
-
-
     <div class="conteneur-enregistrement">
         <div class="forfait_details">
             <h2 id="forfait_titre" class="font-lovelo neon-text"></h2>
@@ -62,30 +61,21 @@
                 <form class="space-y-6" action="{{ route('clients.store') }}" method="POST"
                     enctype="multipart/form-data">
                     @csrf
-
-
                     {{-- CLIENT --}}
                     <div>
-                        <label for="client" class="block text-sm font-medium leading-6 text-gray-900">Client</label>
                         <div class="mt-2 ">
-                            <select
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset select-client"
-                                name="client" id="client">
-                                @foreach ($usagers as $usager)
-                                    @if ($usager->id == Auth::user()->id)
-                                        <option value="{{ $usager->id }}">
-                                            {{ $usager->nom }} {{ $usager->prenom }}
-                                        </option>
-                                    @endif
-                                @endforeach
-                            </select>
+                            @foreach ($usagers as $usager)
+                                @if ($usager->id == Auth::user()->id)
+                                    <h2>{{ $usager->nom }} {{ $usager->prenom }}</h2>
+                                    <input type="hidden" name="client" id="client" value="{{ $usager->id }}">
+                                @endif
+                            @endforeach
                         </div>
                         <x-forms.erreur champ="client" />
                     </div>
-
                     {{-- FORFAIT --}}
                     <div>
-                        <label for="forfait">Forfait</label>
+                        <label for="forfait" class="font-technorace">Forfait</label>
                         <div>
                             <select
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
@@ -104,7 +94,7 @@
                     {{-- DATE ARRIVEE --}}
                     <div>
 
-                        <label for="date_arrivee" class="block text-sm font-medium leading-6 text-gray-900">Date
+                        <label for="date_arrivee" class="font-technorace">Date
                             d'arrivée</label>
                         <div class="mt-2">
                             <input type="date" id="date_arrivee" name="date_arrivee" value="2024-08-19"
@@ -115,7 +105,7 @@
                     {{-- DATE DEPART --}}
                     <div>
 
-                        <label for="date_depart" class="block text-sm font-medium leading-6 text-gray-900">Date de
+                        <label for="date_depart" class="font-technorace">Date de
                             depart
                         </label>
                         <div class="mt-2">
@@ -126,10 +116,8 @@
                     </div>
 
                     {{-- SUBMIT --}}
-                    <div class="mt-2">
-                        <input type="submit"
-                            class="max-w-min m-auto flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            value="Réserver!">
+                    <div class="submit">
+                        <button type="submit" class="submit">Réserver</button>
                     </div>
                 </form>
 
